8160d1d119a5a6186a81285ed370a12e430ad075
Philippe Courtemanche <philippecourtemanche@sansfil-securise-etudiants-lassonde-245-71.polymtl.ca>
2017-02-27 12:35:54 -0500

    class graphe est terminee

--- TP1/Graphe.h
+++ TP1/Graphe.h
@@ -3,9 +3,6 @@ class Graphe{
     
 public:
     void Graphe(string nomFichier);
-   // operator<<(ostream, Graphe); est-ce qu'on implemente?
-    Chemin plusCourtChemin(string, int);
-    Chemin gainMaximal(string, int);
     
 private:
     std::vector<Sommet*> _sommets;
