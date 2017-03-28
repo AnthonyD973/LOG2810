6e8dee8ead2db8eada815c35569d9c7f37957bd0
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-03-27 20:33:45 -0400

    TP2: Déplacement des méthodes de Correction dans Correction.cpp.

--- TP2/Projet_Qt/Modele/ConsoleApplication1/ConsoleApplication1/Correction.h
+++ TP2/Projet_Qt/Modele/ConsoleApplication1/ConsoleApplication1/Correction.h
@@ -20,37 +20,3 @@ private:
 	Lexique* lexique;
 
 };
-
-Correction::Correction()
-{
-}
-
-Correction::~Correction()
-{
-}
-
-std::vector<string> Correction::corrigerMot(string mot) 
-{
-	if (Lexique::getInstance()->existe(mot))
-	{
-		motsCorriges.push_back(mot);;
-	}
-	else
-	{
-		for (int i = 0;i < mot.size();i++)
-		{
-			char reserve = mot[i];
-			for (int j = 0;j < 26;j++)
-			{
-				mot[i] = alphabet[j];
-				if (Lexique::getInstance()->existe(mot))
-					motsCorriges.push_back(mot);
-			}
-			mot[i] = reserve;
-		}
-		
-	}
-	return motsCorriges;
-
-}
-
