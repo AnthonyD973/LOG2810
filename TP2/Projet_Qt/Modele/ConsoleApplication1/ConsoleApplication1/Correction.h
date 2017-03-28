49ebb2fb995f322a14b568a4243dd030f83151bc
Marc HOUNTO <Marc HOUNTO>
2017-03-27 20:27:44 -0400

    derniers ajouts sur le correcteur

--- TP2/Projet_Qt/Modele/ConsoleApplication1/ConsoleApplication1/Correction.h
+++ TP2/Projet_Qt/Modele/ConsoleApplication1/ConsoleApplication1/Correction.h
@@ -2,7 +2,7 @@
 #include"Lexique.h"
 
 
-
+const char alphabet[] =  "qwertyuiopasdfghjklzxcvbnm" ;
 
 
 class Correction
@@ -13,9 +13,11 @@ public:
 
 	vector<string>motsSuggeres;
 	vector<string>motsCorriges;
+	std::vector<string> corrigerMot(string mot);
 
 
 private:
+	Lexique* lexique;
 
 };
 
@@ -26,3 +28,29 @@ Correction::Correction()
 Correction::~Correction()
 {
 }
+
+std::vector<string> Correction::corrigerMot(string mot) 
+{
+	if (Lexique::getInstance()->existe(mot))
+	{
+		motsCorriges.push_back(mot);;
+	}
+	else
+	{
+		for (int i = 0;i < mot.size();i++)
+		{
+			char reserve = mot[i];
+			for (int j = 0;j < 26;j++)
+			{
+				mot[i] = alphabet[j];
+				if (Lexique::getInstance()->existe(mot))
+					motsCorriges.push_back(mot);
+			}
+			mot[i] = reserve;
+		}
+		
+	}
+	return motsCorriges;
+
+}
+
