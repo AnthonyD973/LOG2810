a21b10c65cd052d04a28d6f2d98cbedce8c5db66
Philippe Courtemanche <philippecourtemanche@Sansfil-Securise-Etudiants-Lassonde-246-31.polymtl.ca>
2017-03-27 22:03:52 -0400

    ajout des methodes construireLexique et minimiserLexique, ajout de la classe Node, debut de code pour la methode construireLexique

--- TP2/Projet_Qt/Modele/ConsoleApplication1/ConsoleApplication1/Lexique.cpp
+++ TP2/Projet_Qt/Modele/ConsoleApplication1/ConsoleApplication1/Lexique.cpp
@@ -5,41 +5,59 @@
 
 Lexique::Lexique()
 {
-
+	_racine = new Noeud(false, '\0');
 }
 
 Lexique::~Lexique()
 {
+	delete _racine;
 }
 void Lexique::creerLexique(string & fichier)
 {
-	
+	delete _instance;
+	_instance = new Lexique();
+
+	_construireLexique(fichier, _instance);
+	_minimiserLexique();		
+}
+
+void Lexique::_construireLexique(const string& fichier, Lexique& lexique)
+{
 
 	ifstream donnees(fichier, ios::in);  // 
 
-	if (donnees)  // si l'ouverture a réussi
+	if (donnees)  // si l'ouverture a rï¿½ussi
 	{
-		delete _instance;
-		_instance = new Lexique();
 		// instructions
 		while (!donnees.eof())
 		{
-			for (int i = 0;i < _TOTALELEMENTS;i++)
+			//lire donnees
+			string mot;
+			donnees >> mot;
+			for(int i=0; i<mot.size(); i++) //boucler sur les lettres du mot
 			{
-				getline(donnees, _instance->lexique[i]);
+				if((_instance._racine->getEnfant(mot[i])==nullptr)
+				{
+					//TODO: ajout 
+				}
+				else
+				{
+					//TODO: ce qui se passe quand l'enfant existe deja
+				}
 			}
-
 		}
 
 		donnees.close();  // on ferme le fichier
 	}
 	else  // sinon
 		cout << "Impossible d'ouvrir le fichier !" << endl;
+}
 
-
-
+void Lexique::_minimiserLexique()
+{
 
 }
+
 bool Lexique::existe(string mot)
 {
 	for (int i = 0;i < _TOTALELEMENTS;i++)
--- TP2/Projet_Qt/Modele/ConsoleApplication1/ConsoleApplication1/Lexique.h
+++ TP2/Projet_Qt/Modele/ConsoleApplication1/ConsoleApplication1/Lexique.h
@@ -14,8 +14,19 @@ const int _TOTALELEMENTS = 22000;
 
 class Lexique
 {
+	class Noeud
+	{
+	public:
+		Noeud(bool estFinal, char lettreAssociee);
+		void addEnfant(Noeud* noeud);
+		Noeud* getEnfant(char lettreAssociee); //retourne nullptr si enfant n'existe pas
+	private:
+		vector<Noeud*> _enfants;
+		bool _estFinal;
+		char _lettreAssociee;
+	};
+
 public:
-	
 	~Lexique();
 	bool existe(string mot);
 	vector<string>motsSuggeres;
@@ -26,8 +37,11 @@ public:
 
 private:
 	Lexique();
-	string  lexique[_TOTALELEMENTS];
-	static Lexique*_instance;
+	string lexique[_TOTALELEMENTS];
+	static Lexique* _instance;
+	static void _construireLexique(const string& fichier, Lexique& lexique);
+	void _minimiserLexique();
+	Noeud* _racine;
 
 };
 
